41337ea3f62e6c2df1b2af1a06f7b8389d21e653
DANIELSUPREETH <DANIELSUPREETH>
2024-11-05 22:33:55 +0530

    added two types of inheriatance

--- Sneakers/main.cpp
+++ Sneakers/main.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-class Sneaker {
-private:
+class Footwear {
+protected:
     string name;
     int size;
     string color;
 
 public:
-    static int sneakerCount;
+    Footwear() {}
 
-    Sneaker() {}
-
-    Sneaker(string name, int size, string color) {
+    Footwear(string name, int size, string color) {
         this->name = name;
         this->size = size;
         this->color = color;
-        sneakerCount++;
     }
 
-    ~Sneaker() {
-        sneakerCount--;
+    virtual void printDetails() const {
+        cout << "Footwear: " << name << ", Size: " << size << ", Color: " << color << endl;
     }
 
-    void printDetails() const {
-        cout << "Sneaker: " << name << ", Size: " << size << ", Color: " << color << endl;
-    }
-
-    static int getSneakerCount() {
-        return sneakerCount;
+    string getName() const {
+        return name;
     }
 };
-
-int Sneaker::sneakerCount = 0;
 
 class SneakerBrand {
 private:
@@ -46,7 +38,6 @@ public:
 
     SneakerBrand(string brandName) {
         this->brandName = brandName;
-        brandCount++;
     }
 
     ~SneakerBrand() {
@@ -60,15 +51,63 @@ public:
     static int getBrandCount() {
         return brandCount;
     }
+
+    string getBrandName() const {
+        return brandName;
+    }
 };
 
 int SneakerBrand::brandCount = 0;
 
+class Sneaker : public Footwear, public SneakerBrand {
+public:
+    static int sneakerCount;
+
+    Sneaker() : Footwear(), SneakerBrand() {}
+
+    Sneaker(string name, int size, string color, string brandName) : Footwear(name, size, color), SneakerBrand(brandName) {
+        sneakerCount++;
+        brandCount++;
+    }
+
+    ~Sneaker() {
+        sneakerCount--;
+    }
+
+    void printDetails() {
+        cout << "Sneaker: " << name << ", Size: " << size << ", Color: " << color << ", Brand: " << getBrandName() << endl;
+    }
+
+    static int getSneakerCount() {
+        return sneakerCount;
+    }
+};
+
+int Sneaker::sneakerCount = 0;
+
+class SportShoe : public Footwear {
+public:
+    string sportType;
+
+    SportShoe(string name, int size, string color, string sportType) : Footwear(name, size, color) {
+        this->sportType = sportType;
+    }
+
+    void printDetails() {
+        cout << "Sport Shoe: " << name << ", Size: " << size << ", Color: " << color << ", Sport Type: " << sportType << endl;
+    }
+};
+
 int main() {
+    Footwear footwear[2] = {
+        Footwear("Loafers", 9, "Brown"),
+        Footwear("Flip Flops", 8, "Green")
+    };
+
     Sneaker sneakers[3] = {
-        Sneaker("Dunks", 10, "Red"),
-        Sneaker("Ultra Boost", 9, "Black"),
-        Sneaker("Gel Kayano", 11, "Blue")
+        Sneaker("Dunks", 10, "Red", "Nike"),
+        Sneaker("Ultra Boost", 9, "Black", "Adidas"),
+        Sneaker("Gel Kayano", 11, "Blue", "Asics")
     };
 
     SneakerBrand brands[3] = {
@@ -77,7 +116,23 @@ int main() {
         SneakerBrand("Asics")
     };
 
-    cout << "Sneaker Details:" << endl;
+    SportShoe sportShoes[2] = {
+        SportShoe("Running Shoes", 10, "Black", "Running"),
+        SportShoe("Basketball Shoes", 12, "White", "Basketball")
+    };
+
+    cout << "Sport Shoe Details: " << endl;
+    for (int i = 0; i < 2; ++i) {
+        sportShoes[i].printDetails();
+    }    
+    cout<<endl;
+
+    cout << "Footwear Details:" << endl;
+    for (int i = 0; i < 2; ++i) {
+        footwear[i].printDetails();
+    }
+
+    cout << "\nSneaker Details:" << endl;
     for (int i = 0; i < 3; ++i) {
         sneakers[i].printDetails();
     }
